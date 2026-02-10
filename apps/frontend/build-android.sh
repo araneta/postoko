599bc8228cbb152a661e96607d231b77e16a8323
araneta <adminujianku@gmail.com>
2026-02-10 11:00:10 +0700

    build android

--- apps/frontend/build-android.sh
+++ apps/frontend/build-android.sh
@@ -1,3 +1,6 @@
+#nvm    v22.14.0
+#Gradle: 8.10.2
+
 export ANDROID_SDK_ROOT=/media/araneta/49909430-d2bd-4bcf-be1d-3c425a4013bf/apps/Android/Sdk 
 export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/tools 
 cd android 
