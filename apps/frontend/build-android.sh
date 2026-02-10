f453b4cd9578aef16d7a5257967cc551a71ef48a
araneta <adminujianku@gmail.com>
2026-02-10 10:33:59 +0700

    build frontend

---
+++ apps/frontend/build-android.sh
@@ -0,0 +1,4 @@
+export ANDROID_SDK_ROOT=/media/araneta/49909430-d2bd-4bcf-be1d-3c425a4013bf/apps/Android/Sdk 
+export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/tools 
+cd android 
+./gradlew assembleRelease --no-daemon --max-workers=2
