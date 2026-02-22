162ed1ab21c34bb264b2dde61e89a9b33b166f36
araneta <adminujianku@gmail.com>
2026-02-22 15:23:20 +0700

    git workflow;

--- apps/frontend/build-android.sh
+++ apps/frontend/build-android.sh
@@ -1,7 +1,9 @@
 #nvm    v22.14.0
 #Gradle: 8.10.2
-
+export JAVA_HOME=/media/araneta/49909430-d2bd-4bcf-be1d-3c425a4013bf/apps/jdk-17.0.11
+export PATH=$JAVA_HOME/bin:$PATH
 export ANDROID_SDK_ROOT=/media/araneta/49909430-d2bd-4bcf-be1d-3c425a4013bf/apps/Android/Sdk 
 export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/tools 
-cd android 
+cd android
+./gradlew assembleDebug -PreactNativeArchitectures=arm64-v8a  --no-daemon --max-workers=2
 ./gradlew assembleRelease --no-daemon --max-workers=2
